883f1185e8fa583fe93b402d3052f4848fa2e517
mazajiputra <mazajiputra@gmail.com>
2021-10-29 22:59:58 +0700

    Kumpul Tugas

---
+++ tugas7.js
@@ -0,0 +1,13 @@
+var tinggiBadan=[168,169,170,171,172,173,174,175,176];
+
+for(var i =0;i<tinggiBadan.length;i++){
+    console.log(tinggiBadan[i])
+}
+console.log("")
+
+
+for(let x of tinggiBadan){
+    console.log(x)
+}
+//Setelah di analisis, tidak ada perbedaan pada bagian hasil, 
+//Perbedaan hanya pada panjang program yang diperlukan
